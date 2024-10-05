ed59c246a9163f1aff615d29df5e7736fed63d24
Maxime Moreillon <m.moreillon@gmail.com>
2024-10-06 07:50:19 +0900

    sub as user_id

--- auth.ts
+++ auth.ts
@@ -23,7 +23,7 @@ if (OIDC_JWKS_URI) {
 }
 
 export const getUserId = (req: Request, res: Response) => {
-  const { _id, legacy_id }: any = res.locals.user
+  const { _id, legacy_id, sub }: any = res.locals.user
 
-  return legacy_id || _id
+  return legacy_id || _id || sub
 }
