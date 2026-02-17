9d58ddebc972bf3142f812c2d1742d97a1f9b518
Maxime Moreillon <m.moreillon@gmail.com>
2026-02-17 17:51:31 +0900

    ready for deployment

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { path } = to;
+  // TODO: deal with /api
   if (path !== "/login" && !path.startsWith("/auth")) {
     const session = useUserSession();
     if (!session.user.value) return navigateTo("/login");
