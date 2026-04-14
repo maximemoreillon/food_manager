1e21730e8d09a104f108a0eb912074d3316caed6
Maxime Moreillon <m.moreillon@gmail.com>
2026-04-14 19:29:12 +0900

    improved label parser

--- app/components/food/LabelParsing.vue
+++ app/components/food/LabelParsing.vue
@@ -1,59 +1,38 @@
 <template>
-  <v-dialog max-width="40rem" v-model="dialog">
-    <template v-slot:activator="{ props: activatorProps }">
-      <v-btn
-        v-bind="activatorProps"
-        prepend-icon="mdi-label"
-        text="Parse label"
-        :variant="props.variant"
-      />
-    </template>
-    <template v-slot:default="{ isActive }">
-      <v-card>
-        <v-card-title> Label parsing </v-card-title>
-        <v-card-text>
-          <v-row align="center">
-            <v-col>
-              <v-file-input
-                v-model="image"
-                label="image"
-                accept="image/*"
-                hide-details
-              />
-            </v-col>
-            <v-col cols="auto">
-              <v-btn
-                :disabled="!image"
-                @click="parseLabel()"
-                :loading="loading"
-                color="primary"
-              >
-                <v-icon>mdi-brain</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
+  <v-btn
+    @click="triggerInput"
+    prepend-icon="mdi-label"
+    text="Parse label"
+    :variant="props.variant"
+    :loading="loading"
+  />
 
-          <v-btn text="Close" @click="isActive.value = false"></v-btn>
-        </v-card-actions>
-      </v-card>
-    </template>
-  </v-dialog>
+  <v-file-input
+    ref="fileInput"
+    v-model="image"
+    label="image"
+    accept="image/*"
+    hide-input
+    @update:modelValue="parseLabel()"
+    style="display: none"
+  />
 </template>
 
 <script lang="ts" setup>
 const image = ref<File | null>(null);
-const dialog = ref(false);
 const loading = ref(false);
 const props = defineProps<{
   variant?: "flat" | "text" | "elevated" | "plain" | "tonal" | "outlined";
 }>();
 const emit = defineEmits(["parsed"]);
+const fileInput = ref(null);
+
+function triggerInput() {
+  fileInput.value?.$el.querySelector("input").click();
+}
+
 async function parseLabel() {
   if (!image.value) {
-    alert("Image not provided");
     return;
   }
   loading.value = true;
@@ -63,7 +42,6 @@ async function parseLabel() {
     const data = await $fetch(`/api/label`, { body, method: "POST" });
 
     emit("parsed", data);
-    dialog.value = false;
     image.value = null;
   } catch (error) {
     alert("Label parsing failed");
--- server/openAi.ts
+++ server/openAi.ts
@@ -4,6 +4,6 @@ const { OPENAI_API_KEY } = process.env;
 export let openAiClient: OpenAI;
 
 if (OPENAI_API_KEY) {
-  console.log(`✔ OpenAI API key provided`);
+  console.log("\x1b[32m✔\x1b[0m OpenAI API key provided");
   openAiClient = new OpenAI();
 }
