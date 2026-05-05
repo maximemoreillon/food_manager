21d822e3b940e658f413dbc85d74984798b3770f
Maxime Moreillon <m.moreillon@gmail.com>
2026-05-05 10:17:50 +0900

    api auth no redirect

--- server/utils/getUserId.ts
+++ server/utils/getUserId.ts
@@ -4,9 +4,10 @@ export default async function (event: H3Event<EventHandlerRequest>) {
   const config = useRuntimeConfig();
   if (!config.public.authRequired) return undefined;
 
-  const session = await requireUserSession(event);
+  const { user } = await getUserSession(event);
 
-  const { user } = session;
+  if (!user)
+    throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
 
   return user?.legacy_id || user?.sub;
 }
